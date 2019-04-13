f676ab777546d0cd6a95b4bce023d63ac0c0d505
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2019-04-13 16:09:50 +0200

    Initialized project Created the maven project pom file and added a basic interface for Walsh-Hadamard transformation.

---
+++ src/main/java/hr/fer/zemris/diplproj/Main.java
@@ -0,0 +1,7 @@
+package hr.fer.zemris.diplproj;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/java/hr/fer/zemris/diplproj/walsh/ITransform.java
@@ -0,0 +1,18 @@
+package hr.fer.zemris.diplproj.walsh;
+
+import java.util.List;
+
+/**
+ * Basic interface for the Walsh-Hadamard transform.
+ *
+ * TODO: It could be slow to create a new array for the result, can be solved by reusing an existing one
+ */
+public interface ITransform {
+    /**
+     * Calculates the Walsh-Hadamard transform for the given truth table values.
+     *
+     * @param values The values of a truth table of a boolean function.
+     * @return Coefficients calculated using Walsh-Hadamard transform.
+     */
+    List<Integer> transform(List<Integer> values);
+}
