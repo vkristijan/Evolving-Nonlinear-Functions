713126581368d0401e7bff5a94ff72a197a3d2ca
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2019-04-17 23:41:14 +0200

    Implemented an evaluator to calcualte the function nonlinearity

---
+++ src/main/java/hr/fer/zemris/diplproj/evaluator/NonlinearityEvaluator.java
@@ -0,0 +1,27 @@
+package hr.fer.zemris.diplproj.evaluator;
+
+import hr.fer.zemris.diplproj.BoolFunction;
+
+import java.util.Comparator;
+
+/**
+ * Implementation of an <code>IEvaluator</code> that returns the
+ * nonlinearity of the given boolean function, calculated by its walsh coefficients.
+ *
+ * @author Kristijan Vulinovic
+ */
+public class NonlinearityEvaluator implements IEvaluator {
+    @Override
+    public double evaluate(BoolFunction function) {
+        var maxW = function.getWalshSpectrum()
+                           .stream()
+                           .max(Comparator.naturalOrder());
+
+        if (!maxW.isPresent()) {
+            throw new IllegalStateException("Unable to calculate nonlinearity due to missing walsh coefficients.");
+        }
+
+        int n = function.degree();
+        return (1 << (n - 1)) - maxW.get() / 2.0;
+    }
+}
