129ec740c79939d7b032d158a8e91e46b90dfacf
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2019-04-28 19:21:25 +0200

    Implemented AndFunctionNode Created a function node that calculates the boolean and operation between two child nodes. Implemented appropriate tests.

---
+++ src/test/java/hr/fer/zemris/diplproj/heuristic/genprog/node/function/AndFunctionNodeTest.java
@@ -0,0 +1,34 @@
+package hr.fer.zemris.diplproj.heuristic.genprog.node.function;
+
+import hr.fer.zemris.diplproj.heuristic.genprog.node.INode;
+import hr.fer.zemris.diplproj.heuristic.genprog.node.terminal.ValueTerminalNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AndFunctionNodeTest {
+    @Test
+    public void testAnd(){
+        INode node = new AndFunctionNode();
+        node.addChild(new ValueTerminalNode(0));
+        node.addChild(new ValueTerminalNode(1));
+
+        assertEquals(0, node.evaluate(List.of(0, 0)));
+        assertEquals(0, node.evaluate(List.of(0, 1)));
+        assertEquals(0, node.evaluate(List.of(1, 0)));
+        assertEquals(1, node.evaluate(List.of(1, 1)));
+    }
+
+    @Test
+    public void testWrongChildCount(){
+        INode node = new AndFunctionNode();
+        node.addChild(new ValueTerminalNode(0));
+        node.addChild(new ValueTerminalNode(1));
+
+        assertThrows(IndexOutOfBoundsException.class, () -> node.addChild(new ValueTerminalNode(1)));
+    }
+}
+
