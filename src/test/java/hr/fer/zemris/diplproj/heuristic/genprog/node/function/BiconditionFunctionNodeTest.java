2ffbb0faa408d5478136edaebe055bd7055ab08b
Kristijan Vulinovic <kristijan.vulinovic@outlook.com>
2019-04-28 20:04:15 +0200

    Implemented BiconditionFunctionNode Created a function node that calculates the boolean biconditional operation between two child nodes. Implemented appropriate tests.

---
+++ src/test/java/hr/fer/zemris/diplproj/heuristic/genprog/node/function/BiconditionFunctionNodeTest.java
@@ -0,0 +1,24 @@
+package hr.fer.zemris.diplproj.heuristic.genprog.node.function;
+
+import hr.fer.zemris.diplproj.heuristic.genprog.node.INode;
+import hr.fer.zemris.diplproj.heuristic.genprog.node.terminal.ValueTerminalNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BiconditionFunctionNodeTest {
+    @Test
+    public void testBicondition(){
+        INode node = new BiconditionFunctionNode();
+        node.addChild(new ValueTerminalNode(0));
+        node.addChild(new ValueTerminalNode(1));
+
+        assertEquals(1, node.evaluate(List.of(0, 0)));
+        assertEquals(0, node.evaluate(List.of(0, 1)));
+        assertEquals(0, node.evaluate(List.of(1, 0)));
+        assertEquals(1, node.evaluate(List.of(1, 1)));
+    }
+}
+
